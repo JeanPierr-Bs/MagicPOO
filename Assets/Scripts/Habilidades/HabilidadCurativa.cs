25c4501e88954354998b7ae2b7cc535bf2d57281
andresrios72 <andresrios72@gmail.com>
2025-05-09 15:12:41 -0500

    Ajuste Test
    Se reajusta la clase de Test

--- Assets/Scripts/Habilidades/HabilidadCurativa.cs
+++ Assets/Scripts/Habilidades/HabilidadCurativa.cs
@@ -13,6 +13,6 @@ public class HabilidadCurativa : Habilidad
 
     protected override void Ejecutar()
     {
-        Console.WriteLine($"{Nombre}: cura {CantidadCuracion} puntos de vida.");
+        Debug.Log($"{Nombre}: cura {CantidadCuracion} puntos de vida.");
     }
 }
